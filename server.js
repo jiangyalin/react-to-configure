07f7adc6cbbf5bf833bd8dec4c551be624e32e85
蒋亚林 <18725944157@163.com>
2017-07-16 22:16:56 +0800

    搭建

---
+++ server.js
@@ -0,0 +1,3 @@
+/**
+ * Created by jiang_pc on 2017/7/13.
+ */
---
+++ webpack.dev.js
@@ -0,0 +1,41 @@
+const path = require('path')
+const root = __dirname
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
+
+module.exports = {
+  // 入口文件
+  entry: [
+    'react-hot-loader/patch', // 激活HMR
+    'webpack-dev-server/client',
+    'webpack/hot/only-dev-server',
+    path.resolve(root, 'src/main.js')
+  ],
+  // 出口文件
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(root, 'dist'),
+    publicPath: '/'
+  },
+  devServer: {
+    hot: true, // 激活服务器的HMR
+    contentBase: path.resolve(root, 'dist'),
+    publicPath: '/',
+    port: 8080,
+    historyApiFallback: true
+  },
+  // loaders
+  module: {
+    rules: [
+      {test: /\.jsx?$/, use: ['babel-loader'], exclude: /node_modules/}
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'React Demo',
+      template: path.resolve(root, 'template.html')
+    }),
+    new webpack.HotModuleReplacementPlugin(), // 热替换插件
+    new webpack.NamedModulesPlugin() // 执行热替换时打印模块名字
+  ]
+}
